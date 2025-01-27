bb5ce113d4997a7ef5b31ce308f970ddfb79180a
Bhavyasri837 <nsbhavya83@gmail.com>
2025-01-27 15:47:04 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,15 @@
+class p1
+{
+    public int m1()
+    {
+        int a=10;
+        int b=12;
+        return a+b;
+    }
+    public static void main(String args[])
+    {
+        p1 o=new p1();
+       int res=o.m1();
+       System.out.println(res);
+    }
+}
