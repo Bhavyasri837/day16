fdee3866a12663a5fe737cc5f641c15acf2698b3
Bhavyasri837 <nsbhavya83@gmail.com>
2025-02-10 15:29:10 +0530

    Add files via upload

---
+++ b.java
@@ -0,0 +1,13 @@
+//single level inheritance
+class a
+{
+   static int x=19;
+    static int y=34;
+}
+    class b extends a 
+    {
+    public static void main(String args[])
+    {
+        System.out.println(x+y);
+    }
+    }
